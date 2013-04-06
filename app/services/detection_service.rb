38d0f6f229c75416ea87f93d44b3c3654921c3b7
Martin Lipták <mliptak@gmail.com>
2013-04-06 19:48:21 +0200

    Fix Naive Bayes in category detection.

--- app/services/detection_service.rb
+++ app/services/detection_service.rb
@@ -4,27 +4,20 @@ class DetectionService
 	end
 
 	def category(language)
-		chosen_tokens = @tokens.uniq.first(500)
-		total_words_for_language = TotalDocuments.where(language: language).sum(:number)
-		token_counts = Corpus.for_language(language).where(word: chosen_tokens).map(&:count)
+		chosen_tokens = @tokens.uniq.shuffle.first(200)
+		total_documents_for_language = TotalDocuments.where(language: language).sum(:number)
 		Category.where(language: language).map { |category|
-			total_words_for_language_and_category = TotalDocuments.where(language: language, category_id: category.id).sum(:number)
 			token_counts_for_category = Corpus.for_language(language).where(category_id: category.id, word: chosen_tokens).map(&:count)
+			total_words_for_language_and_category = Corpus.for_language(language).where(category_id: category.id).count
+			total_documents_for_language_and_category = TotalDocuments.where(language: language, category_id: category.id).sum(:number)
 			
 			nominator = token_counts_for_category.inject(0) { |product, token_count_for_category| 
 				product + Math.log(token_count_for_category.to_f / total_words_for_language_and_category) 
-			} * (total_words_for_language_and_category.to_f / total_words_for_language)
+			} + Math.log(total_documents_for_language_and_category.to_f / total_documents_for_language)
 
-			denominator = token_counts.inject(0) { |product, token_count| 
-				product + Math.log(token_count.to_f / total_words_for_language) 
-			}
-			
-			result = nominator / denominator
-			result = 0.0 unless result.finite?
-
-			{ score: result, category: category }
+			{ score: nominator.finite? ? nominator : 0.0, category: category }
 		}.sort_by { |score_category|
 			score_category[:score]
-		}.last[:category]
+		}#.last[:category]
 	end
 end
